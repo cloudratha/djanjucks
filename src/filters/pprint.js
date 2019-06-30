4f059d98ca34abeb2a358dae96c0576f815a61ff
cloudratha <3967435+cloudratha@users.noreply.github.com>
2019-06-30 19:32:50 +0100

    Update sprint filter to use stringify utility.

--- src/filters/pprint.js
+++ src/filters/pprint.js
@@ -1,22 +1,8 @@
 import { runtime } from '..';
-
-// JSON.stringify ignores objects with methods.
-// So recursively inspect to get useful data.
-const parse = item => {
-  if (typeof item === 'function') {
-    return `[Function: ${item.name}]`;
-  } else if (typeof item === 'object') {
-    return Object.keys(item).reduce((accum, key) => {
-      accum[key] = parse(item[key]);
-      return accum;
-    }, {});
-  }
-
-  return item;
-};
+import { stringify } from '../utilities';
 
 const pprint = value => {
-  return runtime.markSafe(JSON.stringify(parse(value), null, 2));
+  return runtime.markSafe(JSON.stringify(stringify(value), null, 2));
 };
 
 export default pprint;
